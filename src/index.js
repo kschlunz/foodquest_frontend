1f32fdefa496f8b11874cbf9d30021fad2a0a3dc
Manuel Grullon <astermanuelg@gmail.com>
2018-05-08 14:42:50 -0400

    clicking on a result centers the map

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+let map;
+
 function initMap() {
   var myLatLng = {
     lat: 40.7051928,
     lng: -74.0138964
   };
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
     center: myLatLng
   });
@@ -30,14 +32,13 @@ function initMap() {
 let ClickEventHandler = function(map, origin) {
   this.origin = origin;
   this.map = map;
+  console.log(map)
   this.placesService = new google.maps.places.PlacesService(map);
-  console.log(this)
   // // Listen for clicks on the map.
   this.map.addListener('click', this.handleClick.bind(this));
 };
 
 ClickEventHandler.prototype.handleClick = function(event) {
-  console.log(event)
   console.log('You clicked on: ' + event.latLng);
   // If the event has a placeId, use it.
   if (event.placeId) {
@@ -48,22 +49,19 @@ ClickEventHandler.prototype.handleClick = function(event) {
     // If you call stop here when there is no placeId you will prevent some
     // other map click event handlers from receiving the event.
     event.stop();
-    this.getPlaceInformation(event.placeId);
+    this.getPlaceInformation(event.placeId, map);
   }
 };
 
-ClickEventHandler.prototype.getPlaceInformation = function(placeId) {
+ClickEventHandler.prototype.getPlaceInformation = function(placeId, map) {
   var me = this;
-  console.log(me, "is me"
-  )
   this.placesService.getDetails({placeId: placeId}, function(place, status) {
     if (status === 'OK') {
       console.log("im inside")
       header = document.createElement("h1")
       header.innerText = `name: ${place.name}, address: ${place.formatted_address} hours: ${place.opening_hours.weekday_text} latlong: ${place.geometry.location}`
       header.addEventListener('click', (event)=> {
-        console.log(map)
-        console.log(map.setCenter)
+        map.setCenter(place.geometry.location)
       })
       document.body.append(header)
     }
