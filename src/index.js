d3ec23a0714dff4e83b36492e66a1dcfd0dd229a
Manuel Grullon <astermanuelg@gmail.com>
2018-05-10 14:02:20 -0400

    added a search bar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 let map
 let markers
 let bounds
+let autocomplete
 
 document.addEventListener("DOMContentLoaded", function(e) {
   let jsFile = document.createElement("script")
@@ -42,41 +43,64 @@ function makeMap(pos) {
     }
   }
   map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 12,
-      center: pos,
-      })
-    //things here get called after the map gets loaded
-    makeListeners()
-    makeSidebar()
-  }
+    zoom: 12,
+    center: pos,
+  })
 
-  function makeSidebar() {
-    fetch("http://localhost:3000/api/v1/users/1/restaurants")
-      .then(res => res.json())
-      .then((res) => {
-        for (let result of res) {
-          restaurant = new Restaurant(result)
-          restaurant.createSideBarItem()
-          setTimeout(restaurant.dropMarker.bind(restaurant), 1000)
-        }
-      })
-  }
 
-  function makeListeners() {
-    placesService = new google.maps.places.PlacesService(map);
-    console.log(map)
-    map.addListener("click", function(event) {
-      //when clicking on a place
-      if (event.placeId) {
-        event.stop()
-        placesService.getDetails({
-          placeId: event.placeId
-        }, function(place, status) {
-          if (status === 'OK') {
-            let restaurant = new Restaurant(place)
-            restaurant.createCardView()
-          }
-        })
+  autocomplete = new google.maps.places.Autocomplete(document.getElementById('search-box'));
+
+  // Bind the map's bounds (viewport) property to the autocomplete object,
+  // so that the autocomplete requests use the current map bounds for the
+  // bounds option in the request.
+  autocomplete.bindTo('bounds', map);
+  //things here get called after the map gets loaded
+  makeListeners()
+  makeSidebar()
+}
+
+function makeSidebar() {
+  fetch("http://localhost:3000/api/v1/users/1/restaurants")
+    .then(res => res.json())
+    .then((res) => {
+      for (let result of res) {
+        restaurant = new Restaurant(result)
+        restaurant.createSideBarItem()
+        setTimeout(restaurant.dropMarker.bind(restaurant), 1000)
       }
     })
-  }
+}
+
+function makeListeners() {
+  placesService = new google.maps.places.PlacesService(map);
+  console.log(map)
+  map.addListener("click", function(event) {
+    //when clicking on a place
+    if (event.placeId) {
+      event.stop()
+      placesService.getDetails({
+        placeId: event.placeId
+      }, function(place, status) {
+        if (status === 'OK') {
+          let restaurant = new Restaurant(place)
+          restaurant.createCardView()
+        }
+      })
+    }
+  })
+  //autocomplete event listener
+  autocomplete.addListener('place_changed', function(event) {
+    let place = autocomplete.getPlace()
+    console.log(place)
+    console.log(this)
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    } else {
+      let restaurant = new Restaurant(place)
+      restaurant.createCardView()
+    }
+  })
+}
