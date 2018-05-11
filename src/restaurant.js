eed49908a117cb8b608e401fd2ab9bb005adaca2
Manuel Grullon <astermanuelg@gmail.com>
2018-05-11 11:20:38 -0400

    fixed delete buttons to show up as remove

--- src/restaurant.js
+++ src/restaurant.js
@@ -167,7 +167,7 @@ class Restaurant {
         document.getElementById('places-want-to-go').appendChild(li)
 
         let deleteButton = document.createElement("button")
-        deleteButton.innerText = "delete"
+        deleteButton.innerText = "remove"
         li.appendChild(deleteButton)
         deleteButton.addEventListener("click", this.deleteRestaurant.bind(this))
         li.addEventListener('click', this.createCardView.bind(this))
@@ -196,7 +196,7 @@ class Restaurant {
         document.getElementById('places-been-to').appendChild(li)
 
         let deleteButton = document.createElement("button")
-        deleteButton.innerText = "delete"
+        deleteButton.innerText = "remove"
         li.appendChild(deleteButton)
         deleteButton.addEventListener("click", this.deleteRestaurant.bind(this))
         li.addEventListener('click', this.createCardView.bind(this))
